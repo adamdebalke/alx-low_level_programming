dbf3dbb59e7d9eb38c5ee3246aa3f576cf11ea10
adamdebalke <youngadam65@gmail.com>
2021-08-31 21:02:57 +0300

    Prints Holberton followed by a new line

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,17 +6,14 @@
  **/
 int main(void)
 {
+	char *c = "_putchar";
+	int i;
 
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
 	_putchar('\n');
-
 	return (0);
+
 }
