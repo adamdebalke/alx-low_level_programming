1adfc7b6672fafeab235dbc08b7ba3b1b3dd004a
adamdebalke <youngadam65@gmail.com>
2021-09-06 12:10:15 +0300

    convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,51 +1,32 @@
 #include "main.h"
-#include <limits.h>
-
 /**
- * _atoi - convert a string to an integer
+ * _atoi - converts a string to an integer.
  * @s: the string to convert
- *
- * Return: result of conversion
+ * Return: the converted string.
  */
 int _atoi(char *s)
 {
-	unsigned int n = 0;
-	int sign = 1;
-	int flag = 0;
+	short boolean;
+	int i, minus, result;
 
-	while (*s)
+	i = minus = result = boolean = 0;
+	minus = -1;
+
+	while (s[i] != '\0')
 	{
-		if (*s >= '0' && *s <= '9')
-		{
-			flag = 1;
-			if (sign > 0)
-			{
-				if (INT_MAX / 10 < (int) n)
-					return (INT_MAX);
-				n *= 10;
-				if (INT_MAX - (*s - '0') < (int) n)
-					return (INT_MAX);
-			}
-			else
-			{
-				if (INT_MIN / 10 < (int) -n)
-					return (INT_MIN);
-				n *= 10;
-				if (INT_MIN + (*s - '0') > (int) -n)
-					return (INT_MIN);
-			}
-			n += (*s - '0');
-		}
-		else if (flag)
-		{
-			break;
-		}
-		else if (*s == '-')
-		{
-			sign *= -1;
-		}
-		++s;
-	}
+		if (s[i] == '-')
+			minus *= -1;
 
-	return (n * sign);
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result -= (s[i] - '0');
+			boolean = 1;
+		}
+		else if (boolean == 1)
+			break;
+		i++;
+	}
+	result *= minus;
+	return (result);
 }
--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <limits.h>
-#include <math.h>
 #include <stdio.h>
 
 /**
@@ -10,75 +8,23 @@
  */
 int main(void)
 {
-	int n;
-	char s_min[12];
-	char s_max[11];
-	char s_lt_min[12];
-	char s_gt_max[11];
-	sprintf(s_min, "%d", INT_MIN);
-	sprintf(s_max, "%d", INT_MAX);
-	sprintf(s_lt_min, "%ld", (long) INT_MIN - 1);
-	sprintf(s_gt_max, "%ld", (long) INT_MAX + 1);
+    int n;
 
-	printf("Converting %s\n", "98");
-	n = _atoi("98");
-	printf("%d\n", n);
-
-	printf("Converting %s\n", "-402");
-	n = _atoi("-402");
-	printf("%d\n", n);
-
-	printf("Converting %s\n", "          ------++++++-----+++++--98");
-	n = _atoi("          ------++++++-----+++++--98");
-	printf("%d\n", n);
-
-	printf("Converting %s\n", "214748364");
-	n = _atoi("214748364");
-	printf("%d\n", n);
-
-	printf("Converting %s\n", "0");
-	n = _atoi("0");
-	printf("%d\n", n);
-
-	printf("Converting %s\n", "Suite 402");
-	n = _atoi("Suite 402");
-	printf("%d\n", n);
-
-	printf("Converting %s\n", "         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-	n = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-	printf("%d\n", n);
-
-	printf("Converting %s\n", "---++++ -++ Sui - te -   402 #cisfun :)");
-	n = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-	printf("%d\n", n);
-
-	printf("Converting %s\n", s_min);
-	n = _atoi(s_min);
-	printf("%d\n", n);
-
-	printf("Converting %s\n", s_max);
-	n = _atoi(s_max);
-	printf("%d\n", n);
-
-	printf("Converting %s\n", s_lt_min);
-	n = _atoi(s_lt_min);
-	printf("%d\n", n);
-
-	printf("Converting %s\n", s_gt_max);
-	n = _atoi(s_gt_max);
-	printf("%d\n", n);
-
-	printf("Converting %s\n", "-4500000000");
-	n = _atoi("-4500000000");
-	printf("%d\n", n);
-
-	printf("Converting %s\n", "4500000000");
-	n = _atoi("4500000000");
-	printf("%d\n", n);
-
-	printf("Converting %s\n", "0 - 1");
-	n = _atoi("0 - 1");
-	printf("%d\n", n);
-
-	return (0);
+    n = _atoi("98");
+    printf("%d\n", n);
+    n = _atoi("-402");
+    printf("%d\n", n);
+    n = _atoi("          ------++++++-----+++++--98");
+    printf("%d\n", n);
+    n = _atoi("214748364");
+    printf("%d\n", n);
+    n = _atoi("0");
+    printf("%d\n", n);
+    n = _atoi("Suite 402");
+    printf("%d\n", n);
+    n = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
+    printf("%d\n", n);
+    n = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+    printf("%d\n", n);
+    return (0);
 }
