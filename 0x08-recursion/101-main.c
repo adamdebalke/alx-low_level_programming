acb95cb58ac16db7e86974a71a0d4dd873ecfbaf
adamdebalke <youngadam65@gmail.com>
2021-09-16 10:38:25 +0300

    calculate length of a string

---
+++ 0x08-recursion/101-main.c
@@ -0,0 +1,111 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str, *pat;
+    int r;
+
+    str = "main.c";
+    pat = "ma**d";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "main.c";
+    pat = "ma**dd";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "";
+    pat = "";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "";
+    pat = "*";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "main.c";
+    pat = "*.*";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "main.c";
+    pat = "*.c";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "main.c";
+    pat = "m*a*i*n*.*c";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "yo-main.c";
+    pat = "m*a*i*n*.*c";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "yo-main.c";
+    pat = "*m*a*i*n*.*c";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "main.c";
+    pat = "m*a*i*n*.*c*";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "main.c";
+    pat = "main.c";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "main.c";
+    pat = "main*.c";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "main.c";
+    pat = "m*c";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "main.c";
+    pat = "m*c*";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "main.c";
+    pat = "ma********************************d";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "main.c";
+    pat = "ma********************************d*";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "main.c";
+    pat = "*";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "main.c";
+    pat = "***";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "main.c";
+    pat = "m.*c";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "main.c";
+    pat = "**.*c";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "main-main.c";
+    pat = "ma*in.c";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "main-main.c";
+    pat = "ma*in.*c";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "main";
+    pat = "main*d";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    str = "abc";
+    pat = "*b";
+    r = wildcmp(str, pat);
+    printf("string : %s\npattern: %s\nmatched: %d\n", str, pat, r);
+    return (0);
+}
