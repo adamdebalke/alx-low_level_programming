c42b4ee168a43f14256f679cbbeb16f92e610c76
adamdebalke <youngadam65@gmail.com>
2021-09-17 11:33:57 +0300

    print a string, followed by a new line

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char);
+void _puts(char *);
+void print_number(int);
+int _atoi(char *);
+int isnumber(char *);
+int subtract_largest_coin(int);
+
+#endif /* HOLBERTON_H */
