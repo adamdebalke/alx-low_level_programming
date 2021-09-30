b77905327c7b3be633246a1e29a3836dffbe79ff
adamdebalke <youngadam65@gmail.com>
2021-09-30 19:17:59 +0300

    select the correct function to perform an operation

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,31 +1,34 @@
+#include <stdio.h>
 #include "3-calc.h"
-#include <string.h>
 
 /**
- * get_op_func - select the correct function to perform an operation
- * @s: the operation to perform
- *
- * Return: If s is not one of the defined operators ('+', '-', '*', '/', '%'),
- * return NULL. Otherwise, return a pointer to the appropriate function.
+ * get_op_func - check if operator
+ * @s: operador
+ * Return: selector.
  */
+
 int (*get_op_func(char *s))(int, int)
 {
-	int i = 0;
+
+
 	op_t ops[] = {
-		{ "+", op_add },
-		{ "-", op_sub },
-		{ "*", op_mul },
-		{ "/", op_div },
-		{ "%", op_mod },
-		{ NULL, NULL }
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
 
-	while (s && ops[i].op != NULL)
-	{
-		if (!strcmp(s, ops[i].op))
-			return (ops[i].f);
-		++i;
-	}
+	int i = 0;
 
+	while (i < 5)
+	{
+		if (*(ops[i].op) == *s)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
 	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,41 @@
 #include "3-calc.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - a basic calculator
- * @argc: the size of the argument vector
- * @argv: the argument vector
- *
- * Description: This program accepts a number, an operator and another number,
- * in that order. It performs the specified operation and prints the result to
- * standard output (followed by a newline). If called with the wrong number of
- * arguments, it prints "Error" and terminates the process with a status of 98.
- * If the operator given is not one of '+', '-', '*', '/' or '%', it prints
- * "Error" and terminates the process with a status of 99. If asked to perform
- * division by 0 (e.g. using the '/' or '%' operators), it prints "Error" and
- * terminates the process with a status value of 100.
- *
- * Return: Always 0
+ * main - program that performs simple operations
+ * @argc: number of command line arguments
+ * @argv: array containing the program command line arguments
+ * Return: 0.
+ * only 3 if argv[1] argv[2] argv[3]
  */
+
 int main(int argc, char *argv[])
 {
-	int (*f)(int, int);
+	int o;
 
 	if (argc != 4)
 	{
-		puts("Error");
+		printf("Error\n");
 		exit(98);
 	}
 
-	f = get_op_func(argv[2]);
-
-	if (!f)
+	if (*argv[2] != '+' &&
+	        *argv[2] != '-' &&
+	        *argv[2] != '*' &&
+	        *argv[2] != '/' &&
+	    *argv[2] != '%')
 	{
-		puts("Error");
+		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
+	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
+	o = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", o);
 	return (0);
 }
