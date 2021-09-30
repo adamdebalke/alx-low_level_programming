420b541feb58b25d9554f1340ae3d5b2fd651fc2
adamdebalke <youngadam65@gmail.com>
2021-09-30 19:33:22 +0300

    print opcodes of this function

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,66 +1,52 @@
-#include "3-calc.h"
-
+#include <stdio.h>
+#include "function_pointers.h"
 /**
  * op_add - add
- * @a: first operand
- * @b: second operand
- * Return: the result of the operation
+ * @a: integer
+ * @b: integer
+ * Return: result.
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
- * op_sub - subtract
- * @a: first operand
- * @b: second operand
- * Return: the result of the operation
+ * op_sub - difference
+ * @a: integer
+ * @b: integer
+ * Return: result
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
- * op_mul - multiply
- * @a: first operand
- * @b: second operand
- * Return: the result of the operation
+ * op_mul - product
+ * @a: integer
+ * @b: integer
+ * Return: result
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
- * op_div - divide
- * @a: first operand
- * @b: second operand
- * Return: the result of the operation
+ * op_div - check if a number is equal to 98
+ * @a: integer
+ * @b: integer
+ * Return: result
  */
 int op_div(int a, int b)
 {
-	if (!b)
-	{
-		puts("Error");
-		exit(100);
-	}
 	return (a / b);
 }
-
 /**
- * op_mod - modulo
- * @a: first operand
- * @b: second operand
- * Return: the result of the operation
+ * op_mod - check if a number is equal to 98
+ * @a: integer
+ * @b: integer
+ * Return: result
  */
 int op_mod(int a, int b)
 {
-	if (!b)
-	{
-		puts("Error");
-		exit(100);
-	}
 	return (a % b);
 }
